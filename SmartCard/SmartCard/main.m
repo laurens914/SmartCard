29f055393e73fa232c692e96b11755a6c22c0ee6
Lauren Spatz <laurens914@gmail.com>
2016-03-21 16:53:30 -0700

    starting project

---
+++ SmartCard/SmartCard/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartCard
+//
+//  Created by Lauren Spatz on 3/21/16.
+//  Copyright © 2016 Lauren Spatz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
