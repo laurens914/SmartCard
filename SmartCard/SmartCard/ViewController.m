39d974511044a7fbe1d6d681c0a76419da74ee77
Vincent Smithers <vsmithers1087@gmail.com>
2016-03-22 14:46:55 -0700

    animated button constraints

--- SmartCard/SmartCard/ViewController.m
+++ SmartCard/SmartCard/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+- (IBAction)create:(UIButton *)sender;
+- (IBAction)save:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *createHeight;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *savedHeight;
 
 @end
 
@@ -18,10 +22,36 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    
+    [self setUpHeightConstrains];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(void)setUpHeightConstrains{
+    _createHeight.constant = self.view.frame.size.height / 2;
+    _savedHeight.constant = self.view.frame.size.height / 2;
+}
+-(void)animateConstraints{
+    [UIView animateWithDuration:1.0 delay:0.2 options:UIViewAnimationOptionCurveEaseIn animations:^{
+        
+        _createHeight.constant = 60.0;
+        _savedHeight.constant = 60.0;
+        
+    } completion:^(BOOL finished) {
+        //
+    }];
+}
+
+- (IBAction)create:(UIButton *)sender {
+}
+
+- (IBAction)save:(UIButton *)sender {
+    [self animateConstraints];
+}
 @end
