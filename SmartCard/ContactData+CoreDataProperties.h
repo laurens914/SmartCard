d218b4aebf16190cc7ae31cf507bbe62e75d5cc2
Lauren Spatz <laurens914@gmail.com>
2016-03-30 13:12:29 -0700

    implemented core data

---
+++ SmartCard/ContactData+CoreDataProperties.h
@@ -0,0 +1,33 @@
+//
+//  ContactData+CoreDataProperties.h
+//  SmartCard
+//
+//  Created by Lauren Spatz on 3/30/16.
+//  Copyright © 2016 Lauren Spatz. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "ContactData.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactData (CoreDataProperties)
+
+@property (nullable, nonatomic, retain) NSString *firstName;
+@property (nullable, nonatomic, retain) NSString *lastName;
+@property (nullable, nonatomic, retain) NSString *phoneNumber;
+@property (nullable, nonatomic, retain) NSString *emailAddress;
+@property (nullable, nonatomic, retain) NSString *buinessName;
+@property (nullable, nonatomic, retain) NSString *positionTitle;
+@property (nullable, nonatomic, retain) NSString *addressStreet;
+@property (nullable, nonatomic, retain) NSString *addressState;
+@property (nullable, nonatomic, retain) NSString *addressCity;
+@property (nullable, nonatomic, retain) NSString *addressPostalCode;
+@property (nullable, nonatomic, retain) NSString *website;
+@property (nullable, nonatomic, retain) NSData *image;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SmartCard/ContactData+CoreDataProperties.m
@@ -0,0 +1,29 @@
+//
+//  ContactData+CoreDataProperties.m
+//  SmartCard
+//
+//  Created by Lauren Spatz on 3/30/16.
+//  Copyright © 2016 Lauren Spatz. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "ContactData+CoreDataProperties.h"
+
+@implementation ContactData (CoreDataProperties)
+
+@dynamic firstName;
+@dynamic lastName;
+@dynamic phoneNumber;
+@dynamic emailAddress;
+@dynamic buinessName;
+@dynamic positionTitle;
+@dynamic addressStreet;
+@dynamic addressState;
+@dynamic addressCity;
+@dynamic addressPostalCode;
+@dynamic website;
+@dynamic image;
+
+@end
